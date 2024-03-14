80bc56745797ff41162683c0a0392c90864bf84a
- <deadm7us@mail.ru>
2024-03-14 13:39:14 +0300

    add dockerfile,docker-compose and config for github workflow

--- config/data.py
+++ config/data.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 class Data:
     def __init__(self, driver):
         self.driver = driver
--- pages/personal_page.py
+++ pages/personal_page.py
@@ -8,13 +8,12 @@ class PersonalPage(BasePage):
     PAGE_URL = Links.PERSONAL_PAGE
 
     FIRST_NAME_FIELD = ("xpath", "//input[@name='firstName']")
-    SAVE_BUTTON = ("xpath", "(//btton[@type='submit'])[1]")
+    SAVE_BUTTON = ("xpath", "(//button[@type='submit'])[1]")
 
     def change_name(self, new_name):
         with allure.step(f"Change name on '{new_name}'"):
             first_name_field = self.wait.until(EC.element_to_be_clickable(self.FIRST_NAME_FIELD))
             first_name_field.clear()
-            assert first_name_field.get_attribute("value") == '', "There is text"
             first_name_field.send_keys(new_name)
             self.name = new_name
 
@@ -24,4 +23,4 @@ class PersonalPage(BasePage):
 
     @allure.step('Changes has been saved successfully')
     def is_changes_saved(self):
-        self.wait.until(EC.text_to_be_present_in_element(self.FIRST_NAME_FIELD, self.name))
+        self.wait.until(EC.text_to_be_present_in_element_value(self.FIRST_NAME_FIELD, self.name))
